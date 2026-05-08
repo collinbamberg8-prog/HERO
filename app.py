f6439ec9b312510dc9927914c04c176b046b6c1b
collinbamberg8-prog <collinbamberg8@gmail.com>
2026-05-08 09:10:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Hero AI", page_icon="🌑")
 st.title("🌑 Hero")
 
 # ENTER YOUR API KEY HERE
-API_KEY = "PASTE_YOUR_AIZA_KEY_HERE" 
+API_KEY = "AIzaSyAE9j4iwcN0qqFl0uwSCB7ykBiYu8mabTc" 
 genai.configure(api_key=API_KEY)
 
 # --- 2. MODEL INITIALIZATION ---
